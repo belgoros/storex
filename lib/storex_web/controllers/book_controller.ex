7a1bd8cd15688202d7697bbb7bbfc1a215f82f58
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-10-10 17:37:17 +0200

    Display a dynamic list of books

--- lib/storex_web/controllers/book_controller.ex
+++ lib/storex_web/controllers/book_controller.ex
@@ -2,6 +2,39 @@ defmodule StorexWeb.BookController do
   use StorexWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", books: list_books()
+  end
+
+  def list_books() do
+    [
+      %{
+        id: "1",
+        title: "My first book",
+        description: "My first story",
+        price: "15.9",
+        image_url: "http://www.phoenixforrailsdevelopers.com/books/1.png"
+        },
+        %{
+          id: "2",
+          title: "My second book",
+          description: "My first story",
+          price: "25.9",
+          image_url: "http://www.phoenixforrailsdevelopers.com/books/2.png"
+        },
+        %{
+          id: "3",
+          title: "My third book",
+          description: "My first story",
+          price: "35.9",
+          image_url: "http://www.phoenixforrailsdevelopers.com/books/3.png"
+        },
+        %{
+          id: "4",
+          title: "My fourth book",
+          description: "My first story",
+          price: "45.9",
+          image_url: "http://www.phoenixforrailsdevelopers.com/books/4.png"
+        }
+    ]
   end
 end
