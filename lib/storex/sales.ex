87865a75e365eea9af2352481ea11bee3059211c
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-10-14 17:28:47 +0200

    Create and run line_items migration

--- lib/storex/sales.ex
+++ lib/storex/sales.ex
@@ -101,4 +101,100 @@ defmodule Storex.Sales do
   def change_cart(%Cart{} = cart) do
     Cart.changeset(cart, %{})
   end
+
+  alias Storex.Sales.LineItem
+
+  @doc """
+  Returns the list of sales_line_items.
+
+  ## Examples
+
+      iex> list_sales_line_items()
+      [%LineItem{}, ...]
+
+  """
+  def list_sales_line_items do
+    Repo.all(LineItem)
+  end
+
+  @doc """
+  Gets a single line_item.
+
+  Raises `Ecto.NoResultsError` if the Line item does not exist.
+
+  ## Examples
+
+      iex> get_line_item!(123)
+      %LineItem{}
+
+      iex> get_line_item!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_line_item!(id), do: Repo.get!(LineItem, id)
+
+  @doc """
+  Creates a line_item.
+
+  ## Examples
+
+      iex> create_line_item(%{field: value})
+      {:ok, %LineItem{}}
+
+      iex> create_line_item(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_line_item(attrs \\ %{}) do
+    %LineItem{}
+    |> LineItem.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a line_item.
+
+  ## Examples
+
+      iex> update_line_item(line_item, %{field: new_value})
+      {:ok, %LineItem{}}
+
+      iex> update_line_item(line_item, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_line_item(%LineItem{} = line_item, attrs) do
+    line_item
+    |> LineItem.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a LineItem.
+
+  ## Examples
+
+      iex> delete_line_item(line_item)
+      {:ok, %LineItem{}}
+
+      iex> delete_line_item(line_item)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_line_item(%LineItem{} = line_item) do
+    Repo.delete(line_item)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking line_item changes.
+
+  ## Examples
+
+      iex> change_line_item(line_item)
+      %Ecto.Changeset{source: %LineItem{}}
+
+  """
+  def change_line_item(%LineItem{} = line_item) do
+    LineItem.changeset(line_item, %{})
+  end
 end
--- test/storex/sales_test.exs
+++ test/storex/sales_test.exs
@@ -59,4 +59,63 @@ defmodule Storex.SalesTest do
       assert %Ecto.Changeset{} = Sales.change_cart(cart)
     end
   end
+
+  describe "sales_line_items" do
+    alias Storex.Sales.LineItem
+
+    @valid_attrs %{quantity: 42}
+    @update_attrs %{quantity: 43}
+    @invalid_attrs %{quantity: nil}
+
+    def line_item_fixture(attrs \\ %{}) do
+      {:ok, line_item} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Sales.create_line_item()
+
+      line_item
+    end
+
+    test "list_sales_line_items/0 returns all sales_line_items" do
+      line_item = line_item_fixture()
+      assert Sales.list_sales_line_items() == [line_item]
+    end
+
+    test "get_line_item!/1 returns the line_item with given id" do
+      line_item = line_item_fixture()
+      assert Sales.get_line_item!(line_item.id) == line_item
+    end
+
+    test "create_line_item/1 with valid data creates a line_item" do
+      assert {:ok, %LineItem{} = line_item} = Sales.create_line_item(@valid_attrs)
+      assert line_item.quantity == 42
+    end
+
+    test "create_line_item/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Sales.create_line_item(@invalid_attrs)
+    end
+
+    test "update_line_item/2 with valid data updates the line_item" do
+      line_item = line_item_fixture()
+      assert {:ok, %LineItem{} = line_item} = Sales.update_line_item(line_item, @update_attrs)
+      assert line_item.quantity == 43
+    end
+
+    test "update_line_item/2 with invalid data returns error changeset" do
+      line_item = line_item_fixture()
+      assert {:error, %Ecto.Changeset{}} = Sales.update_line_item(line_item, @invalid_attrs)
+      assert line_item == Sales.get_line_item!(line_item.id)
+    end
+
+    test "delete_line_item/1 deletes the line_item" do
+      line_item = line_item_fixture()
+      assert {:ok, %LineItem{}} = Sales.delete_line_item(line_item)
+      assert_raise Ecto.NoResultsError, fn -> Sales.get_line_item!(line_item.id) end
+    end
+
+    test "change_line_item/1 returns a line_item changeset" do
+      line_item = line_item_fixture()
+      assert %Ecto.Changeset{} = Sales.change_line_item(line_item)
+    end
+  end
 end
