674ab622594989eee7e95cf0510674717de8526a
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-10-15 15:40:48 +0200

    Implement Sales context tests

--- lib/storex/sales.ex
+++ lib/storex/sales.ex
@@ -3,5 +3,56 @@ defmodule Storex.Sales do
   alias Storex.Repo
   alias Storex.Sales.{Cart, LineItem}
 
+  def create_cart(attrs \\%{}) do
+    %Cart{}
+    |> Cart.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def get_cart!(id) do
+    Repo.get!(Cart, id)
+  end
+
+  def add_book_to_cart(book, cart) do
+    line_item = Repo.get_by(LineItem, book_id: book.id, cart_id: cart.id)
+    if line_item do
+      line_item
+      |> LineItem.changeset(%{quantity: line_item.quantity + 1})
+      |> Repo.update()
+    else
+      %LineItem{book_id: book.id, cart_id: cart.id}
+      |> LineItem.changeset(%{quantity: 1})
+      |> Repo.insert()
+    end
+  end
+
+  def remove_book_from_cart(book, cart) do
+    line_item = Repo.get_by!(LineItem, book_id: book.id, cart_id: cart.id)
+    if line_item.quantity > 1 do
+      line_item
+      |> LineItem.changeset(%{quantity: line_item.quantity - 1})
+      |> Repo.update()
+    else
+      Repo.delete!(line_item)
+    end
+  end
+
+  def list_line_items(cart) do
+    LineItem
+    |> where(cart_id: ^cart.id)
+    |> preload(:book)
+    |> Repo.all()
+  end
+
+  def line_items_quantity_count(items) do
+    Enum.reduce(items, 0, fn(item, acc) -> acc + item.quantity end)
+  end
+
+  def line_items_total_price(items) do
+    Enum.reduce(items, Decimal.new(0), fn(item, acc) ->
+      quantity = Decimal.new(item.quantity)
+      partial = Decimal.mult(quantity, item.book.price)
+      Decimal.add(acc, partial)
+    end)
   end
 end
