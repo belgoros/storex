ee9888826262a8992faf2d32cee30d9ae04c16c1
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-10-16 16:15:21 +0200

    Format the code

--- lib/storex_web/plugs/cart.ex
+++ lib/storex_web/plugs/cart.ex
@@ -4,16 +4,19 @@ defmodule StorexWeb.Plugs.Cart do
 
   @session_name :cart_id
   @assign_name :cart
-  
+
   def init(opts), do: opts
 
   def call(conn, _opts) do
     cart_id = get_session(conn, @session_name)
+
     cond do
       cart = cart_id && Sales.get_cart!(cart_id) ->
         assign(conn, @assign_name, cart)
+
       true ->
         {:ok, cart} = Sales.create_cart()
+
         conn
         |> put_session(@session_name, cart.id)
         |> assign(@assign_name, cart)
