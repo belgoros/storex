26ec767f4c48cdf635ca25fcba2174a0cb6caf25
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-10-17 10:57:24 +0200

    Implement #get_user/1 and new_user/0
    - update Accounts context
    - add new tests

--- lib/storex/accounts/accounts.ex
+++ lib/storex/accounts/accounts.ex
@@ -8,4 +8,12 @@ defmodule Storex.Accounts do
     |> User.changeset(attrs)
     |> Repo.insert()
   end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
+  end
+
+  def new_user() do
+    User.changeset(%User{}, %{})
+  end
 end
--- test/storex/accounts/accounts_test.exs
+++ test/storex/accounts/accounts_test.exs
@@ -40,5 +40,36 @@ defmodule Storex.AccountsTest do
       {:error, changeset} = Accounts.create_user(duplicated_email_attrs)
       assert "has already been taken" in errors_on(changeset).email
     end
+
+    test "get_user!/1 returns a user" do
+      fixture = user_fixture()
+      user = Accounts.get_user!(fixture.id)
+      assert user.id == fixture.id
+    end
+
+    test "new_user/0 returns an empty changeset" do
+      assert %Ecto.Changeset{} = Accounts.new_user()
+    end
+
+    test "authenticate_user/2 returns a user when email and password match" do
+      user_fixture()
+
+      assert {:ok, %User{}} =
+               Accounts.authenticate_user(@valid_attrs.email, @valid_attrs.password)
+    end
+
+    test "authenticate_user/2 returns an error when email is not found" do
+      user_fixture()
+
+      assert {:error, "invalid user­identifier"} =
+               Accounts.authenticate_user("invalid@test.tld", @valid_attrs.password)
+    end
+
+    test "authenticate_user/2 returns an error when password doesn't match" do
+      user_fixture()
+
+      assert {:error, "invalid password"} =
+               Accounts.authenticate_user(@valid_attrs.email, "invalid")
+    end
   end
 end
